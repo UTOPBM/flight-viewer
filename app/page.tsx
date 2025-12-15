b550120980a3b169971015b39da1ad8ce8eb594e
Your Name <ds5909@naver.com>
2025-12-15 15:03:15 +0900

    fix: use static OG image to resolve static export build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,13 @@
 import type { Metadata } from 'next'
 import HomeClient from './HomeClient'
 
-type Props = {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
-
-export async function generateMetadata(
-  props: Props
-): Promise<Metadata> {
-  const searchParams = await props.searchParams
-  const search = searchParams.search
-  let imageIndex = 1
-
-  if (typeof search === 'string' && search.length > 0) {
-    // Simple hash: sum of char codes % 3 + 1
-    let sum = 0
-    for (let i = 0; i < search.length; i++) {
-      sum += search.charCodeAt(i)
-    }
-    imageIndex = (sum % 3) + 1
-  } else {
-    // Random for root page
-    imageIndex = Math.floor(Math.random() * 3) + 1
-  }
-
-  return {
-    openGraph: {
-      images: [`/images/og/og-${imageIndex}.jpg`],
-    },
-    twitter: {
-      card: 'summary_large_image',
-      images: [`/images/og/og-${imageIndex}.jpg`],
-    }
+export const metadata: Metadata = {
+  openGraph: {
+    images: ['/images/og/og-1.jpg'],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    images: ['/images/og/og-1.jpg'],
   }
 }
 
